bdd666acafb8ab1a9b937350b9a6c0af5b3f6b5b
il720506 <il720506@gmail.com>
2023-05-04 14:53:10 +0300

    Now button is work. Trubble is on self.ui.dir.clicked.connect(self.open_directory_dialog()). No "()" in called func. (open_directory_dialog)

--- MainWindow.py
+++ MainWindow.py
@@ -16,40 +16,35 @@ class MainWindow(QMainWindow):
         # self.directory_list = QListWidget(self)
 
         # self.ui.translit_button.setShortcut('Ctrl+t')
-        self.ui.dir.clicked.connect(self.test)
-        self.ui.test.clicked.connect(self.test2)
+        self.ui.dir.clicked.connect(self.open_directory_dialog)
+        self.ui.test.clicked.connect(self.open_file_dialog)
         # self.ui.files.clicked.connect(self.open_file_dialog())
 
         # clear button
         # self.ui.clear_button.setShortcut('Ctrl+d')
         # self.ui.clear_button.clicked.connect(self.clear_all_windows)
 
-    def test(self):
-        self.ui.out.setText(self.ui.out.text() + '*')
-        print('button on')
-
-    def test2(self):
-        print('**********')
-
     def open_directory_dialog(self):
         dialog = QFileDialog(self)
         dialog.setDirectory(r'F:')
         dialog.setFileMode(QFileDialog.FileMode.Directory)
         dialog.setViewMode(QFileDialog.ViewMode.List)
-        filenames = dialog.selectedFiles()
-        # dialog.exec()
-        if filenames:
-            self.directory_list.append(filenames)
-        print(self.directory_list)
+        if dialog.exec():
+            filenames = dialog.selectedFiles()
+            if filenames:
+                self.directory_list.append(filenames)
+                print(self.directory_list)
+                # self.ui.out.setText(*self.directory_list)
+                # self.ui.out.setText(self.ui.out.text() + '@')
 
-    # def open_file_dialog(self):
-    #     dialog = QFileDialog(self)
-    #     dialog.setDirectory(r'F:')
-    #     dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
-    #     # dialog.setNameFilter("Images (*.png *.jpg)")
-    #     dialog.setViewMode(QFileDialog.ViewMode.List)
-    #     if dialog.exec():
-    #         filenames = dialog.selectedFiles()
-    #         if filenames:
-    #             self.file_list.addItems([str(Path(filename)) for filename in filenames])
-    #     print(self.file_list)
+    def open_file_dialog(self):
+        dialog = QFileDialog(self)
+        dialog.setDirectory(r'F:')
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        # dialog.setNameFilter("Images (*.png *.jpg)")
+        dialog.setViewMode(QFileDialog.ViewMode.List)
+        if dialog.exec():
+            filenames = dialog.selectedFiles()
+            if filenames:
+                self.file_list.addItems([str(Path(filename)) for filename in filenames])
+        print(self.file_list)
