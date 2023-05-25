852fcb5c0e214b46001c3d2f3bc1b5f9a652ade4
il720506 <il720506@gmail.com>
2023-05-25 12:09:45 +0300

    Edit main_base file. Add some function to work with main base dict

--- main_base.py
+++ main_base.py
@@ -1,3 +1,17 @@
 class MianBase:
     def __init__(self):
+        self.all_items: dict = {}
+        self.list_of_file: list[str] = []
+        self.name_item: str = ""
+
+    @staticmethod
+    def check_name(name_: str, dict_: dict):
+        if name_ in dict_:
+            return True
+        return False
+
+    def add_item(self) -> None:
+        pass
+
+    def save_base(self):
         pass
