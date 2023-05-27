e0ef9bf253598d61746c528b6b992e4f11751da8
il720506 <il720506@gmail.com>
2023-05-27 17:29:43 +0300

    Create New repository with name "EasyBack" and copy il_BackUp here

--- d_MainWindow.py
+++ d_MainWindow.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
 
-################################################################################
-## Form generated from reading UI file 'ui_MainWindow.ui'
-##
-## Created by: Qt User Interface Compiler version 6.5.0
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
                             QMetaObject, QObject, QPoint, QRect,
