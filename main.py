64ca48f027ff4fc3b045f890471ccba4a2bb1085
il720506 <il720506@gmail.com>
2023-06-03 14:29:50 +0300

    Start working with "first check" function

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import winreg
 from PySide6.QtWidgets import QApplication
 from MainWindow import MainWindow
 
+
 # from MainWindow import MainWindow
 
-if __name__ == '__main__':
 
+def first_time_check():
+    try:
+        reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                 r'SOFTWARE\EasyBack',
+                                 0, winreg.KEY_WRITE)
+        print(reg_key)
+    except FileNotFoundError:
+        pass
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
--- reg_ttttttt.py
+++ reg_ttttttt.py
@@ -1,22 +1,25 @@
 import winreg
 
 KEY = winreg.HKEY_LOCAL_MACHINE
-REG_PATH: str = r'SOFTWARE\EasyBack'
+SUBKEY_PATH: str = r'SOFTWARE\EasyBack'
+KEY_NAME: str = 'settings_path'
+KEY_VALUE: str = r'f:\!_____back_test\SETTINGS'
 
 
-def create_key(key, subkey):
+def create_key(key, subkey: str, name: str, value: str) -> None:
     try:
         rez = winreg.OpenKey(key, subkey, 0, winreg.KEY_WRITE)
     except FileNotFoundError:
         winreg.CreateKey(key, subkey)
         rez = winreg.OpenKey(key, subkey, 0, winreg.KEY_WRITE)
-        winreg.SetValueEx(rez, 'settings', 0, winreg.REG_SZ, 'exist')
-    print(rez)
-    winreg.CloseKey(rez)
+        winreg.SetValueEx(rez, name, 0, winreg.REG_SZ, value)
+        winreg.CloseKey(rez)
 
 
-def delete_key():
-    pass
+def delete_key(key, subkey: str) -> None:
+    winreg.DeleteKey(key, subkey)
+    # print(f"key HKEY_LOCAL_MACHINE\\{subkey}{KEY_NAME} was deleted from regitry")
 
 
-create_key(KEY, REG_PATH)
+create_key(KEY, SUBKEY_PATH, KEY_NAME, KEY_VALUE)
+# delete_key(KEY, SUBKEY_PATH)
