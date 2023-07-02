124da2855a9f99c68e47f68c11609e5161409a58
il720506 <il720506@gmail.com>
2023-07-02 16:06:05 +0300

    Settings edit

--- all_styles.py
+++ all_styles.py
@@ -1,61 +1,66 @@
 # from main_base import MainBase
 
-SETTINGS_MAIN = "*{" \
-                "background-color: rgb(30, 30, 30);" \
-                "font: 16pt \"Lexend Light\";" \
-                "border: 1px solid;" \
-                "border-color: #2B79C2;" \
-                "color: rgb(230, 230, 230)}" \
-                "QLabel{" \
-                "font: 12pt \"Lexend Light\";" \
-                "color: #2B79C2;" \
-                "border: no}" \
-                "QLineEdit{" \
-                "background-color: rgb(30, 30, 30);}" \
-                "QLineEdit:disabled{" \
-                "background-color: rgb(50,50,50);}" \
-                "QFrame{" \
-                "border-color: #2B79C2;}" \
-                "QPushButton {" \
-                "border: 2px solid;" \
-                "color: rgb(230, 230, 230);" \
-                "border-color: rgb(110, 110, 110);" \
-                "border-radius: 15px;" \
-                "background-color: rgba(60,60, 60, 80);}" \
-                "QPushButton:disabled{" \
-                "background-color: rgb(100,100,100);}" \
-                "QPushButton:hover {" \
-                "color: #2B79C2;" \
-                "border: 3px solid;" \
-                "background-color: rgba(30, 30, 30, 180);" \
-                "border-color: rgb(150,150, 150);}" \
-                "QPushButton:pressed {" \
-                "color: rgb(30, 30, 30);" \
-                "border: 2px solid;" \
-                "background-color: #2B79C2;" \
-                "border-color: rgb(230, 230, 230);}" \
-                "QRadioButton{" \
-                "font: 14pt \"Lexend Light\";" \
-                "border: no}" \
-                "QToolButton{" \
-                "image: url(:/icon/icons/GREY/info_invert.svg);" \
-                "border: no}" \
-                "QToolButton:hover {" \
-                "image: url(:/icon/icons/GREY/info.svg);" \
-                "border: no}" \
-                "QToolButton:pressed{" \
-                "image: url(:/icon/icons/GREY/info_invert.svg);" \
-                "border: no}" \
-                "QTabBar::tab {" \
-                "background:  rgb(60,60, 60);" \
-                "font: 12pt \"Lexend Light\";" \
-                "color: rgb(150,150, 150);}" \
-                "QTabBar::tab:selected {" \
-                "background: #2B79C2;" \
-                "color: rgb(230, 230, 230)}" \
-                "QFrame:disabled{ background-color: rgb(50,50,50);}" \
-                "QRadioButton:disabled{ background-color: rgb(50,50,50);}" \
-                "QToolButton:disabled{ background-color: rgb(50,50,50);}"
+SETTINGS_MAIN = """
+            *{
+                background-color: rgb(30, 30, 30);
+                font: 16pt \"Lexend Light\";
+                border: 1px solid;
+                border-color: #2B79C2;
+                color: rgb(230, 230, 230)}
+            QLabel{
+                font: 12pt \"Lexend Light\";
+                color: #2B79C2;
+                border: no}
+            QLineEdit{
+                background-color: rgb(30, 30, 30);}
+            QLineEdit:disabled{
+                background-color: rgb(50,50,50);}
+            QFrame{
+                border-color: #2B79C2;}
+            QPushButton {
+                border: 2px solid;
+                color: rgb(230, 230, 230);
+                border-color: rgb(110, 110, 110);
+                border-radius: 15px;
+                background-color: rgba(60,60, 60, 80);}
+            QPushButton:disabled{
+                background-color: rgb(100,100,100);}
+            QPushButton:hover {
+                color: #2B79C2;
+                border: 3px solid;
+                background-color: rgba(30, 30, 30, 180);
+                border-color: rgb(150,150, 150);}
+            QPushButton:pressed {
+                color: rgb(30, 30, 30);
+                border: 2px solid;
+                background-color: #2B79C2;
+                border-color: rgb(230, 230, 230);}
+            QRadioButton{
+                font: 14pt \"Lexend Light\";
+                border: no}
+            QToolButton{
+                image: url(:/icon/icons/GREY/info_invert.svg);
+                border: no}
+            QToolButton:hover {
+                image: url(:/icon/icons/GREY/info.svg);
+                border: no}
+            QToolButton:pressed{
+                image: url(:/icon/icons/GREY/info_invert.svg);
+                border: no}
+            QTabBar::tab {
+                background:  rgb(60,60, 60);
+                font: 12pt \"Lexend Light\";
+                color: rgb(150,150, 150);}
+            QTabBar::tab:selected {
+                background: #2B79C2;
+                color: rgb(230, 230, 230)}
+            QFrame:disabled{
+                background-color: rgb(50,50,50);}
+            QRadioButton:disabled{
+                background-color: rgb(50,50,50);}
+            QToolButton:disabled{
+            background-color: rgb(50,50,50);}
+            """
 
 MSG_PUSH_BUTTON: str = """
             QPushButton { 
@@ -80,11 +85,6 @@ MSG_PUSH_BUTTON: str = """
                 }
                 """
 
-# TODO !!!DON`T REMEMBER CLEAR THIS!!!!
-# TODO ??? And why don`t work????
-# MSG_MAIN: str = f'background-color: rgb(30, 30, 30);' \
-#                 f'color: rgb(230, 230, 230);' \
-#                 f'font: {MainBase.font_size_dialog}\"Lexend Light\";'
 
 PROGRESS_BAR_LINE: str = """
             QWidget {
--- d__07_settings.py
+++ d__07_settings.py
@@ -8,7 +8,7 @@ from PySide6.QtWidgets import (QColorDialog, QComboBox, QFileDialog, QFrame, QHB
 import dop_win_rc
 from main_base import MainBase
 import MainWindow as mw
-import all_styles
+from all_styles import SETTINGS_MAIN
 
 
 class SettingsDialog(object):
@@ -23,66 +23,7 @@ class SettingsDialog(object):
         Dialog.setSizePolicy(sizePolicy)
         Dialog.setMinimumSize(QSize(620, 600))
         Dialog.setMaximumSize(QSize(620, 602))
-        Dialog.setStyleSheet(u"*{\n"
-                             "background-color: rgb(30, 30, 30);\n"
-                             "font: 16pt \"Lexend Light\";\n"
-                             "border: 1px solid;\n"
-                             "border-color: #2B79C2;\n"
-                             "color: rgb(230, 230, 230)}\n"
-                             "QLabel{\n"
-                             "font: 12pt \"Lexend Light\";\n"
-                             "color: #2B79C2;\n"
-                             "border: no}\n"
-                             "QLineEdit{\n"
-                             "background-color: rgb(30, 30, 30);}\n"
-                             "QLineEdit:disabled{\n"
-                             "background-color: rgb(50,50,50);}\n"
-                             "QFrame{\n"
-                             "border-color: #2B79C2;}\n"
-                             "QPushButton {\n"
-                             "border: 2px solid;\n"
-                             "color: rgb(230, 230, 230);\n"
-                             "border-color: rgb(110, 110, 110);\n"
-                             "border-radius: 15px;\n"
-                             "background-color: rgba(60,60, 60, 80);}\n"
-                             "QPushButton:disabled{\n"
-                             "background-color: rgb(100,100,100);}\n"
-                             "QPushButton:hover {\n"
-                             "color: #2B79C2;\n"
-                             "border: 3px solid;\n"
-                             "background-color: rgba(30, 30, 30, 180);\n"
-                             "border-color: rgb(150,150, 150);}\n"
-                             "QPushButton:pressed {\n"
-                             "color: rgb(30, 30, 30);\n"
-                             "border: 2px solid;\n"
-                             "background-color: #2B79C2;\n"
-                             "border-color: rgb(230, 230, 230);}\n"
-                             "QRadioButton{\n"
-                             "font: 14pt \"Lexend Light\";\n"
-                             "border: no}\n"
-                             "QToolButton{\n"
-                             ""
-                             "image: url(:/icon/icons/GREY/info_invert.svg);\n"
-                             "border: no}\n"
-                             "QToolButton:hover {\n"
-                             "image: url(:/icon/icons/GREY/info.svg);\n"
-                             "border: no}\n"
-                             "QToolButton:pressed{\n"
-                             "image: url(:/icon/icons/GREY/info_invert.svg);\n"
-                             "border: no}\n"
-                             "QTabBar::tab {\n"
-                             "background:  rgb(60,60, 60);\n"
-                             "font: 12pt \"Lexend Light\";\n"
-                             "color: rgb(150,150, 150);}\n"
-                             "QTabBar::tab:selected {\n"
-                             "background: #2B79C2;\n"
-                             "color: rgb(230, 230, 230)}\n"
-                             "QFrame:disabled{\n"
-                             "background-color: rgb(50,50,50);}\n"
-                             "QRadioButton:disabled{\n"
-                             "background-color: rgb(50,50,50);}\n"
-                             "QToolButton:disabled{\n"
-                             "background-color: rgb(50,50,50);}")
+        Dialog.setStyleSheet(SETTINGS_MAIN)
         self.verticalLayout_4 = QVBoxLayout(Dialog)
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.tabWidget = QTabWidget(Dialog)
